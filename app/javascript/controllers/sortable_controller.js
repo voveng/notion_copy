b5f7111b917c2c849196555b09cb222afcb82032
Vladimir <ostapenkov74@gmail.com>
2025-10-23 15:37:15 +0500

    Fix sortable

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,54 +3,43 @@ import Sortable from "sortablejs";
 import { post } from "@rails/request.js";
 
 export default class extends Controller {
-  static Values = { sortUrl: String };
+  static values = { url: String };
 
   connect() {
     this.initSortable();
   }
-  
+
   initSortable() {
-    // Destroy existing sortable instance if it exists
     if (this.sortable) {
       this.sortable.destroy();
     }
-    
+
     this.sortable = Sortable.create(this.element, {
       animation: 150,
       handle: ".handle",
-      dataIdAttr: "data-id",  // Use data-id attribute to identify items
-      ghostClass: "sortable-ghost", // Class name for the drop placeholder
-      chosenClass: "sortable-chosen", // Class name for the chosen item
-      dragClass: "sortable-drag", // Class name for the dragged element
-      onChoose: (event) => {
-        console.log("Item chosen for dragging", event.item);
-      },
+      dataIdAttr: "data-id",
+      ghostClass: "sortable-ghost",
+      chosenClass: "sortable-chosen",
+      dragClass: "sortable-drag",
       onStart: (event) => {
-        console.log("Drag started", event.item);
-        // Set cursor to grabbing when drag starts
-        document.body.style.cursor = 'grabbing';
+        document.body.style.cursor = "grabbing";
       },
       onEnd: (event) => {
-        console.log("Drag ended", event.item, event.newIndex, event.oldIndex);
-        // Reset cursor when drag ends
-        document.body.style.cursor = '';
+        document.body.style.cursor = "";
         const serialized = this.sortable.toArray();
-        console.log("sortable", serialized);
         this.postData(serialized);
       },
     });
-    console.log("sortable", this.sortable);
   }
-  
+
   disconnect() {
-    // Clean up sortable instance when controller disconnects
     if (this.sortable) {
       this.sortable.destroy();
     }
   }
 
   async postData(serialized) {
-    const url = this.sortUrlValue;
+    const url = this.urlValue;
 
     const response = await post(url, {
       body: JSON.stringify(serialized),
@@ -59,7 +48,6 @@ export default class extends Controller {
       },
     });
     if (response.ok) {
-      console.log("response", response);
     } else {
       console.error("Sorting failed", response);
     }
